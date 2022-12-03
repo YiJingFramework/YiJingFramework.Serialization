5edb2f58c9279877b728d05860f9ce95d3e93ed6
yueyinqiu <lujiaqi2003@126.com>
2022-12-03 22:24:22 +0800

    upld

---
+++ YiJingFramework.Serialization/YiJingFramework.Serialization/IStringConvertibleForJson.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace YiJingFramework.Serialization
+{
+    public interface IStringConvertibleForJson<TSelf> where TSelf : IStringConvertibleForJson<TSelf>
+    {
+        abstract static bool FromStringForJson(string s, [MaybeNullWhen(false)] out TSelf result);
+
+        string ToStringForJson();
+    }
+}
---
+++ YiJingFramework.Serialization/YiJingFramework.Serialization/JsonConverterOfStringConvertibleForJson.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace YiJingFramework.Serialization
+{
+    public sealed class JsonConverterOfStringConvertibleForJson<T> : JsonConverter<T>
+        where T : IStringConvertibleForJson<T>
+    {
+        public override bool HandleNull => false;
+        
+        public override T Read(
+            ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.String)
+                throw new JsonException();
+
+            var s = reader.GetString();
+            Debug.Assert(s is not null);
+            if (!T.FromStringForJson(s, out var result))
+                throw new JsonException();
+            return result;
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToStringForJson());
+        }
+    }
+}
